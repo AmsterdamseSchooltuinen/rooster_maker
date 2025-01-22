0226a8d19a0b5f531fc51abbd419b441d20ae04d
Amelia Lindroth Henriksson | ADC <alindroth@amsterdamdatacollective.com>
2025-01-22 13:57:48 +0100

    Added solver and conatrints functions

---
+++ src/constraint_functions.py
@@ -0,0 +1,14 @@
+
+def add_constraints(garden: Garden):
+    add_constraint1(garden)
+    add_constraint2(garden)
+    # etc...
+    return
+
+
+def add_constraint1(garden: Garden):
+    return
+
+
+def add_constraint2(garden: Garden):
+    return
