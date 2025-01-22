938fa61a09cd88eeb2f5d3570b090ebd778b6628
nils <nils@acmetric.org>
2025-01-22 13:10:40 +0000

    Refactor Streamlit integration: remove unused code and add validation exception handling

--- src/streamlit.py
+++ src/streamlit.py
@@ -1,27 +1,4 @@
-import streamlit as st
-import pandas as pd
-
-def do_streamlit():
-
-    title_label = "Goede morgen!"
-    educators_label = "Leraren Beschikbaarheid Data"
-    school_label = "Groep Beschikbaarheid Data"
-    garden_label = "Schooltuinen Data"
-
-    st.title(title_label)
-
-
-    educators_bytes = st.file_uploader(educators_label)
-    school_bytes = st.file_uploader(school_label)
-    garden_bytes = st.file_uploader(garden_label)
-
-    st.divider()
-    status = st.button("Go!", use_container_width=True)
-    st.divider()
-
-    if status:
-        st.write("Alright!")
-
-
-
-do_streamlit()
+class ValidationException(Exception):
+    def __init__(self, message, input_name):
+        super().__init__(message)
+        self.input_name = input_name
